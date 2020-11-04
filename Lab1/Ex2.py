446f26d668fb4095ccfdc8db35a59df83fbbd23c
MauriVass <maurizio140333@gmail.com>
2020-11-04 21:14:39 +0100

    Refactor Lab1 Ex2.py to oop

--- Lab1/Ex2.py
+++ Lab1/Ex2.py
@@ -3,7 +3,11 @@ Python script which records an audio file using a Raspberry Pi module and a micr
 Wiring:
 -Connect the mic to an usb port
 Input:
--The program requires 3 parameters: the sampling rate (in Hz), the resolution, the name of the output file
+-The program requires 4 parameters:
+	--the duration of the registration (in s)
+	--the sampling rate (in Hz)
+	--the resolution (8, 16, 32)
+	--the name of the output file
 Output:
 -A .wav file
 '''
@@ -14,68 +18,87 @@ import os
 import argparse
 import time
 
-#Create a parser obj to get input parameters
-parser = argparse.ArgumentParser()
-parser.add_argument('--rate', type=int, help='sampling rate in Hz', required=True)
-parser.add_argument('--resolution', type=int, help='resolution=(8, 16, 32)', required=True)
-parser.add_argument('-o', type=str, help='output', required=True)
-args = parser.parse_args()
+class Mic:
+	def __init__(self,dur,rate,res,output):
+		#Get the pre-set resolution values using pyaudio library
+		self.input_res = int(res)
+		if(self.input_res==8):
+			self.resolution = pyaudio.paInt8
+		elif(self.input_res==16):
+			self.resolution = pyaudio.paInt16
+		elif(self.input_res==32):
+			self.resolution = pyaudio.paInt32
+		else:
+			raise ValueError
 
-#Get the pre-set resolution values using pyaudio library
-input_res = args.resolution
-if(input_res==8):
-	resolution = pyaudio.paInt8
-elif(input_res==16):
-	resolution = pyaudio.paInt16
-elif(input_res==32):
-	resolution = pyaudio.paInt32
-else:
-	raise ValueError
+		#Set variables for recording
+		self.channels = 1
+		self.rate = rate
+		#This is the amount of memory that can be stored on the ram while recording
+		#After this memory is full, it will be freed storing the samples on the HD
+		self.chunk = 4800
+		self.record_seconds = dur
+		self.output_file = output
 
-#Set variables for recording
-channels = 1
-rate = args.rate
-chunk = 4800
-record_seconds = 1
-output_file = args.o
+	def Record(self):
+		audio = pyaudio.PyAudio()
 
-audio = pyaudio.PyAudio()
+		stream = audio.open(format=self.resolution,
+			channels=self.channels,
+			rate= self.rate, input=True,
+			frames_per_buffer=self.chunk)
+		print('Recording')
+		frames = []
 
-stream = audio.open(format=resolution,
-	channels=channels,
-	rate= rate, input=True,
-	frames_per_buffer=chunk)
-print('Recording')
-frames = []
+		max_val = int(self.rate / self.chunk * self.record_seconds)
+		time_start = time.time()
+		for i in range(max_val):
+			data = stream.read(self.chunk, exception_on_overflow=False)
+			frames.append(data)
 
-max_val = int(rate / chunk * record_seconds)
-time_start = time.time()
-for i in range(max_val):
-	data = stream.read(chunk, exception_on_overflow=False)
-	frames.append(data)
+		time_end = time.time()
+		elapsed_time = time_end - time_start
+		print(f'Finished Recording. Elapsed time {elapsed_time:.3f}')
 
-time_end = time.time()
-elapsed_time = time_end - time_start
-print(f'Finished Recording. Elapsed time {elapsed_time:.3f}')
+		#Close streaming and destroy audio obj to free the memory
+		stream.stop_stream()
+		stream.close()
+		audio.terminate()
 
-#Close streaming and destroy audio obj to free the memory
-stream.stop_stream()
-stream.close()
-audio.terminate()
+		time_start = time.time()
+		#Save file as a binary file
+		waveFile = wave.open(self.output_file,'wb')
+		waveFile.setnchannels(self.channels)
+		waveFile.setsampwidth(audio.get_sample_size(self.resolution))
+		waveFile.setframerate(self.rate)
+		#Merge all the recorded frames as binary
+		waveFile.writeframes(b''.join(frames))
+		waveFile.close()
+		time_end = time.time()
 
-time_start = time.time()
-#Save file as a binary file
-waveFile = wave.open(output_file,'wb')
-waveFile.setnchannels(channels)
-waveFile.setsampwidth(audio.get_sample_size(resolution))
-waveFile.setframerate(rate)
-#Merge all the recorded frames as binary
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
-time_end = time.time()
+		storage_time = time_end - time_start
+		print(f'Time to storage {storage_time:.3f}')
 
-storage_time = time_end - time_start
-print(f'Time to storage {storage_time:.3f}')
+		#Print the file size value: higher resolution means higher file size
+		print(f'File Size {os.path.getsize(self.output_file)/10**3}KB')
 
-#Print the file size value: higher resolution means higher file size
-print(f'File Size {os.path.getsize(output_file)}')
+if __name__=='__main__':
+	#Create a parser obj to get input parameters
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--duration', type=int, help='duration in s', required=True)
+	parser.add_argument('--rate', type=int, help='sampling rate in Hz (check your microphone specific)', required=True)
+	parser.add_argument('--resolution', type=int, help='resolution=(8, 16, 32)', required=True)
+	parser.add_argument('-o', type=str, help='output', required=True)
+	args = parser.parse_args()
+
+        #Save the input parameters in variables
+	duration = args.duration
+	rate = args.rate
+	resolution = args.resolution
+	file_name = args.o
+
+	#Create microphone instance
+	microphone = Mic(duration,rate,resolution,file_name)
+	#Start recording
+	microphone.Record()
+
