9dde5882f39589a103cb9d6ca44ebeea9b2a1357
MauriVass <maurizio140333@gmail.com>
2020-11-06 08:48:49 +0100

    Refactor to oop

--- Lab2/Ex1.py
+++ Lab2/Ex1.py
@@ -2,7 +2,7 @@
 Python script which resample an audio file from an high frequency to a lower one.
 This is useful to reduce the file size without losing to much information.
 Input:
--The program requires 1 parameters: an input .wav file
+-The program requires 2 parameters: an input .wav file, the name of the output file (optional)
 Output:
 -A .wav file
 '''
@@ -11,46 +11,61 @@ from scipy.io import wavfile
 from scipy import signal
 import numpy as np
 import os
-from playsound import playsound
 import simpleaudio as sa
 import argparse
+import time
 
-#Receive input parameter
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', type=str, help='input .wav file', required=True)
-args = parser.parse_args()
+class Resampler:
 
-def play_sound(path):
-	print(f'Playing {path}')
-	wave_obj = sa.WaveObject.from_wave_file(path)
-	play_obj = wave_obj.play()
-	play_obj.wait_done()
+	def play_sound(self,path):
+		print(f'Playing {path}')
+		wave_obj = sa.WaveObject.from_wave_file(path)
+		play_obj = wave_obj.play()
+		play_obj.wait_done()
 
-input_file = args.i
-rate, audio = wavfile.read(input_file)
+	def Resample(self,input,output):
+		input_file = input
+		#rate: the frequency of the audio
+		#audio: numpy array containing the audio data
+		rate, audio = wavfile.read(input_file)
 
-#Print the value of the input file
-initial_size = os.path.getsize(input_file)
-print(f'Initial size {initial_size}')
+		#Print the value of the input file
+		initial_size = os.path.getsize(input_file)
+		print(f'Initial size {initial_size}')
 
-#Frequency used to resample the audio file.
-#This value should a number such that the value ratio=init_freq/sampling_freq is integer.
-#In this case the init_frq is 48k and sampling_rate is 16k
-sampling_freq = 16000
-ratio = rate / sampling_freq
-audio = signal.resample_poly(audio,1,ratio)
+		#Frequency used to resample the audio file.
+		#This value should a number such that the value ratio=init_freq/sampling_freq is integer.
+		#In this case the init_frq is 48k and sampling_rate is 16k
+		sampling_freq = 16000
+		ratio = rate / sampling_freq
 
-audio = audio.astype(np.int16)
+		start_time = time.time()
+		audio = signal.resample_poly(audio,1,ratio)
+		end_time = time.time()
+		exec_time = end_time - start_time
+		print(f'Elapsed time {exec_time:.3f}')
+		audio = audio.astype(np.int16)
 
-#Save the resampled audio
-output_file = 'output.wav'
-wavfile.write(output_file,sampling_freq,audio)
+		#Save the resampled audio
+		output_file = output
+		wavfile.write(output_file,sampling_freq,audio)
 
-#Print the value of the output file (should be lower the the input size)
-final_size = os.path.getsize(output_file)
-print(f'Initial size {final_size}')
+		#Print the value of the output file (should be lower the the input size)
+		final_size = os.path.getsize(output_file)
+		print(f'Initial size {final_size}')
 
-#Play both file to check if they (almost) are the same
-print('Playing..')
-play_sound(input_file)
-play_sound(output_file)
+if __name__=='__main__':
+
+	#Receive input parameter
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', type=str, help='input .wav file', required=True)
+	parser.add_argument('-o', type=str, help='output .wav file', required=False)
+	args = parser.parse_args()
+
+	resampler = Resampler()
+	resampler.Resample(args.i,args.o)
+
+	#Play both file to check if they (almost) are the same
+	print('Playing..')
+	resampler.play_sound(args.i)
+	resampler.play_sound(args.o)
