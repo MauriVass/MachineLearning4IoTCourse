24157df5f2c34d38c43a6900df080724bb1aadf3
MauriVass <maurizio140333@gmail.com>
2020-11-06 09:31:31 +0100

    Refactor to oop

--- Lab2/Ex3.py
+++ Lab2/Ex3.py
@@ -1,41 +1,59 @@
+'''
+Python script that calculate the Mel-Frequency Cepstral Coefficients(MFCC) of a given spectrogram.
+Input:
+-The program requires 2 parameters: an input .spect file, the name of the output file
+Output:
+-A .mfccs file
+'''
+
 import tensorflow as tf
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i',type=str, help='input spectrogram file', required=True)
-args = parser.parse_args()
+class MFCC:
+	def __init__(self,mel_bins,sampling_rate,low_freq,up_freq,verbose=False):
+		self.num_mel_bins = mel_bins
+		self.sampling_rate = sampling_rate
+		self.lower_frequency = low_freq
+		self.upper_frequency = up_freq
+		self.verbose=verbose
 
-input_file = args.i
-spectrogram = tf.io.read_file(input_file)
-spectrogram = tf.io.parse_tensor(spectrogram,out_type=tf.float32)
+	def CalculateMFCC(self,input_file,output_file):
+		spectrogram = tf.io.read_file(input_file)
+		spectrogram = tf.io.parse_tensor(spectrogram,out_type=tf.float32)
 
-num_mel_bins = 40
-sampling_rate = 16000
-lower_frequency = 20
-upper_frequency = 4000
-print(spectrogram.shape)
-num_spectrogram_bins = spectrogram.shape[-1]
-print(num_spectrogram_bins)
-linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-			num_mel_bins,
-			num_spectrogram_bins,
-			sampling_rate,
-			lower_frequency,
-			upper_frequency)
-mel_spectrogram = tf.tensordot(spectrogram,linear_to_mel_weight_matrix,1)
-mel_spectrogram.set_shape(spectrogram.shape[:-1].concatenate(
-			linear_to_mel_weight_matrix.shape[-1:]))
-log_mel_spectrogram = tf.math.log(mel_spectrogram + 1e-6)
+		#print(spectrogram.shape)
+		num_spectrogram_bins = spectrogram.shape[-1]
+		#print(num_spectrogram_bins)
+		linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+					self.num_mel_bins,
+					num_spectrogram_bins,
+					self.sampling_rate,
+					self.lower_frequency,
+					self.upper_frequency)
+		mel_spectrogram = tf.tensordot(spectrogram,linear_to_mel_weight_matrix,1)
+		mel_spectrogram.set_shape(spectrogram.shape[:-1].concatenate(
+					linear_to_mel_weight_matrix.shape[-1:]))
+		log_mel_spectrogram = tf.math.log(mel_spectrogram + 1e-6)
 
-mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[:,:10]
+		mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[:,:10]
 
-output_file = f'output_{input_file[:-6]}_Ex3.mfccs'
-print(f'Saved {output_file}')
-file = open(output_file,'w')
-print(mfccs.numpy(),file=file)# tf.write_file
-file.close()
-file_inp_size = os.path.getsize(input_file)
-print(f'File size {file_inp_size}')
-file_out_size = os.path.getsize(output_file)
-print(f'File size {file_out_size}')
+		if(self.verbose):
+			print(f'Saved {output_file}')
+		file = open(output_file,'w')
+		print(mfccs.numpy(),file=file)
+		file.close()
+		if(self.verbose):
+			file_inp_size = os.path.getsize(input_file)
+			print(f'File size {file_inp_size}')
+			file_out_size = os.path.getsize(output_file)
+			print(f'File size {file_out_size}')
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i',type=str, help='input spectrogram file', required=True)
+	parser.add_argument('-o',type=str, help='output file', required=True)
+	args = parser.parse_args()
+
+	mfcc = MFCC(40,16000,20,4000)
+	mfcc.CalculateMFCC(args.i,args.o)
