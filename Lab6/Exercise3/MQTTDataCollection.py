678cd2631c3f461ec48edae332463658def0a528
MauriVass <maurizio140333@gmail.com>
2020-12-29 16:47:24 +0100

    Temp and Humi working

--- Lab6/Exercise3/MQTTDataCollection.py
+++ Lab6/Exercise3/MQTTDataCollection.py
@@ -11,12 +11,13 @@ import sys
 sys.path.insert(0, './../Exercise1')
 from MyMQTT import MyMQTT
 
+
 class DoSomething():
 	def __init__(self, clientID):
 		# create an instance of MyMQTT class
 		self.clientID = clientID
-		self.myMqttClient = MyMQTT(self.clientID, "mqtt.eclipseprojects.io", 1883, self) 
-		
+		self.myMqttClient = MyMQTT(self.clientID, "mqtt.eclipseprojects.io", 1883, self)
+
 		self.record_audio = False
 
 	def run(self):
@@ -30,23 +31,26 @@ class DoSomething():
 		self.myMqttClient.stop ()
 
 	def notify(self, topic, msg):
-		# manage here your received message. You can perform some error-check here  
-		r = msg.decode('utf-8')
-		r = json.loads(r)
-		print(f"Record Audio")
-		self.record_audio = True
+		# manage here your received message. You can perform some error-check here
+		if(topic==self.myMqttClient._topic):
+			r = msg.decode('utf-8')
+			r = json.loads(r)
+			print(f"Record Audio ", topic, r)
+			self.record_audio = True
 
 class DataCollector():
 	def __init__(self):
 		self.dht_device = adafruit_dht.DHT11(D4)
 
-		self.audio = pyaudio.PyAudio()
-		self.stream = self.audio.open(format=pyaudio.paInt16,
-			channels=1,
-			rate= 48000, input=True,
-			frames_per_buffer=4800)
-		self.stream.wait()
-		
+		self.is_mic_plugged = False
+		if(self.is_mic_plugged):
+			self.audio = pyaudio.PyAudio()
+			self.stream = self.audio.open(format=pyaudio.paInt16,
+				channels=1,
+				rate= 48000, input=True,
+				frames_per_buffer=4800)
+			self.stream.wait()
+
 
 	def GetTemp(self):
 		#Collect Temp and Hum
@@ -58,30 +62,31 @@ class DataCollector():
 		return humidity
 
 	def GetAudio(self):
-		#Record file audio
-		frames = []
-		self.stream.start_stream()
-		for i in range(10):
-			data = stream.read(4800) #, exception_on_overflow=False)
-			frames.append(data)
-		self.stream.stop_stream()
-		#It is needed to send data over network since you can't send raw bytes
-		audio_64_bytes = base64.b64encode(b''.join(frames))
-		audio_string = audio_b64bytes.decode()
-		
-		return audio_string
+		if(self.is_mic_plugged):
+			#Record file audio
+			frames = []
+			self.stream.start_stream()
+			for i in range(10):
+				data = stream.read(4800) #, exception_on_overflow=False)
+				frames.append(data)
+			self.stream.stop_stream()
+			#It is needed to send data over network since you can't send raw bytes
+			audio_64_bytes = base64.b64encode(b''.join(frames))
+			audio_string = audio_b64bytes.decode()
+			return audio_string
+
 
 if __name__ == "__main__":
 	test = DoSomething("publisher 1")
 	test.run()
-	test.myMqttClient.mySubscribe("/ASD123/date/time/timestamp/Sensor1/")
+	test.myMqttClient.mySubscribe("/ASD123/date/time/timestampa/Sensor1/Audio/")
 	dc = DataCollector()
 
 	a = 0
 
 	#This is used to publish or not the timestamp (==2 -> publish)
 	counter = 2
-	while (a < 2000):
+	while (a < 4):
 		date_time = datetime.datetime.now()
 		timestamp = datetime.datetime.timestamp(date_time)
 
@@ -93,7 +98,7 @@ if __name__ == "__main__":
 		test.myMqttClient.myPublish ("/ASD123/date/time/", json.dumps(message),False)
 		message['timestamp'] = timestamp
 		test.myMqttClient.myPublish ("/ASD123/date/time/timestamp/", json.dumps(message),False)
-		
+
 
 		events = []
 		temperature = dc.GetTemp()
@@ -112,7 +117,7 @@ if __name__ == "__main__":
 		ip = '2.44.137.33' + '/'
 		body = {
 					'bn' : 'http://'+ip,
-					'bi' : timestamp,
+					'bi' : int(timestamp),
 					'e' : events
 				}
 
